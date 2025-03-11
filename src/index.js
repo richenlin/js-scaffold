444e076cc2b1e49e6349efad8cd828d8503aeeab
richenlin <richenlin@gmail.com>
2025-03-11 11:18:55 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Usage: 
+ * @Author: richen
+ * @Date: 2025-03-11 11:11:14
+ * @LastEditTime: 2025-03-11 11:11:23
+ * @License: BSD (3-Clause)
+ * @Copyright (c): <richenlin(at)gmail.com>
+ */
+
+
+/**
+ * test
+ * @param {*} params 
+ * @returns 
+ */
+export function Test(params) {
+  return params;
+}
