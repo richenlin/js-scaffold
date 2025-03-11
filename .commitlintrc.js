884a0b7cda0c1d4e907ccee455b13a4ecb8b9d3c
richenlin <richenlin@gmail.com>
2025-03-11 11:55:42 +0800

    build: 调整配置文件以规范提交信息和代码风格

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'
-  ],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-      2, 'always',
-      ['init', 'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'build', 'ci', 'revert'
-      ]
+      2,
+      'always',
+      [
+        'init',
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'build',
+        'ci',
+        'revert',
+      ],
     ],
-    'type-empty': [
-      2, 'never'
-    ],
-    'scope-enum': [
-      0
-    ],  // 不校验scope类型
-    'scope-empty': [
-      0
-    ],  // 不校验scope是否设置
-    'subject-case': [
-      0
-    ],  // 不校验描述的字符格式
-    'subject-min-length': [
-      2, 'always',
-      2
-    ],  // 描述至少5个字符
+    'type-empty': [2, 'never'],
+    'scope-enum': [0], // 不校验scope类型
+    'scope-empty': [0], // 不校验scope是否设置
+    'subject-case': [0], // 不校验描述的字符格式
+    'subject-min-length': [2, 'always', 2], // 描述至少5个字符
   },
 };
